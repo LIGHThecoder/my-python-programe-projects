98960da11584c68c0a59b88b1ec4e21df2f33709
LIGHThecoder <159998200+LIGHThecoder@users.noreply.github.com>
2024-02-16 23:13:18 +0530

    xx

--- 02_making ansimple AI who wishes you according to time.py
+++ 02_making ansimple AI who wishes you according to time.py
@@ -10,3 +10,45 @@ Topic : making an programe that wish you according to time.
 
 '''
 
+# programe to show time current time at input
+
+import time
+
+showtime=int(time.strftime("%H"))
+# print(showtime)
+
+raw=input("what's going on? how are you\n")
+
+
+if(showtime<=12):
+    print("ok! Good MORNING brother. hope you will gave great time")
+
+elif(showtime>12 and showtime<=18):
+    print("ok! Good AFTERNOON brother. hope you will gave great time")
+
+else:
+    print("ok! Good EVENING brother. hope you will gave great time")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03_5 students result making.py
@@ -0,0 +1,67 @@
+
+
+'''
+author: Nikhil rajendra Gaikwad
+
+date : 16-02-24
+
+'''
+
+#small programe that shows 5 students result in oder to under match case statements also to use if else statements in it
+
+rollno=int(input("enter your Roll Number"))
+
+match rollno:
+    case 1:
+        print("welcome Nikhil\n ")
+        resultwhich=int(input("click 1 to see sem1 result and click 2 for sem 2 result"))
+        # input=int(resultwhich)
+        if(resultwhich==1):
+            print("ok so here are your same 1 result")
+            print("maths=30/50\nscience=40/50,\nmarathi=20/50,\nhistory=45/50,\ngeography = 24/50")
+
+        elif(resultwhich==2):
+            print("ok here are your sem 2 result")
+            print("maths=32/50\nscience=48/50,\nmarathi=21/50,\nhistory=42/50,\ngeography = 29/50")
+
+    case 2:
+        print("welcome rahul\n")
+        resultwhich=int(input("click 1 to see sem1 result and click 2 for sem 2 result"))
+
+        # input=int(resultwhich)
+        if(resultwhich==1):
+            print("ok so here are your same 1 result")
+            print("maths=30/50\nscience=40/50,\nmarathi=20/50,\nhistory=45/50,\ngeography = 24/50")
+
+        elif(resultwhich==2):
+            print("ok here are your sem 2 result")
+            print("maths=32/50\nscience=48/50,\nmarathi=21/50,\nhistory=42/50,\ngeography = 29/50")
+        
+    case 3:
+        print("welcome vinit\n")
+        resultwhich=int(input("click 1 to see sem1 result and click 2 for sem 2 result"))
+
+        # input=int(resultwhich)
+        if(resultwhich==1):
+            print("ok so here are your same 1 result")
+            print("maths=30/50\nscience=40/50,\nmarathi=20/50,\nhistory=45/50,\ngeography = 24/50")
+        # resultwhich=int(input("click 1 to see sem1 result and click 2 for sem 2 result"))
+
+
+        elif(resultwhich==2):
+            print("ok here are your sem 2 result")
+            print("maths=32/50\nscience=48/50,\nmarathi=21/50,\nhistory=42/50,\ngeography = 29/50")
+        
+
+    case 4:
+        print("welcome chris\n")
+        resultwhich=int(input("click 1 to see sem1 result and click 2 for sem 2 result"))
+
+        # input=int(resultwhich)
+        if(resultwhich==1):
+            print("ok so here are your same 1 result")
+            print("maths=30/50\nscience=40/50,\nmarathi=20/50,\nhistory=45/50,\ngeography = 24/50")
+
+        elif(resultwhich==2):
+            print("ok here are your sem 2 result")
+            print("maths=32/50\nscience=48/50,\nmarathi=21/50,\nhistory=42/50,\ngeography = 29/50")        
