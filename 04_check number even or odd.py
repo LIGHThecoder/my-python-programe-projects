3c091f3304589b7b6d4d6efb78cf602b68d724f7
LIGHThecoder <159998200+LIGHThecoder@users.noreply.github.com>
2024-02-19 17:25:10 +0530

    completer of project no 4

---
+++ 04_check number even or odd.py
@@ -0,0 +1,26 @@
+'''
+    Author : Nikhil Rajendra Gaikwad
+    
+    project: to check the given number is odd or even
+    
+    date : 19-02-24
+    
+     '''
+
+
+# a=2
+
+b=int(input("input the value in order to check it is even or odd \n"))
+if (b % 2==0):
+    print("this is even number")
+
+else:
+    print("this is an odd number")
+
+    # / this means upper ans value in division in upper that is quotient
+    # % this means lower ans valun in division mean the remainder which is written in very down side on when we do division
+
+
+    '''
+    /(division) means quotient
+    %(modulous) means  remainder'''
